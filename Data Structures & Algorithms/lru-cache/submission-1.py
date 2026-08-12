dc7dede1fd4e5dfce46595f3e5c2434cc619cdf3
Lucian (Luka) Lavric <151498964+lucianlavric@users.noreply.github.com>
2026-08-12 13:19:41 -0400

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.py
@@ -0,0 +1,77 @@
+class ListNode:
+    def __init__(self):
+        self.nxt = self.prev = None
+        self.val = self.key = None
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.Hmap = {}
+        self.left = ListNode()
+        self.right = ListNode()
+        self.left.nxt = self.right
+        self.right.prev = self.left
+        self.capacity = capacity
+        self.count = 0
+
+        # right = LRU
+        # left = MRU
+        
+
+    def get(self, key: int) -> int:
+        # if it exists
+        # remove node from LL
+        if key in self.Hmap:
+            self.remove(self.Hmap[key])
+            self.insert(self.Hmap[key])
+            return self.Hmap[key].val
+        # else return 1
+        return -1
+
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.Hmap:
+            # if the key already exists, we just update its value
+            self.Hmap[key].val = value
+            self.remove(self.Hmap[key])
+            self.insert(self.Hmap[key])
+        elif self.count == self.capacity:
+            to_remove = self.right.prev
+            #remove lru from Hmap
+            del self.Hmap[to_remove.key]
+            # remove lru from LL
+            self.remove(to_remove)
+
+            # add new key to LL
+            node = ListNode()
+            node.key = key
+            node.val = value
+            self.Hmap[key] = node
+            self.insert(node)
+        else:
+            node = ListNode()
+            node.key = key
+            node.val = value
+            self.Hmap[key] = node
+            self.insert(node)
+            self.count += 1
+
+    
+    def insert(self, node: ListNode):
+        prev = self.left
+        nxt = self.left.nxt
+        node.prev = prev
+        node.nxt = nxt
+        nxt.prev = prev.nxt = node
+
+
+    
+    def remove(self, node: ListNode):
+        prev = node.prev
+        nxt = node.nxt
+        prev.nxt = nxt
+        nxt.prev = prev
+
+        
